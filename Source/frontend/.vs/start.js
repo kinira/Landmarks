f23f29e95f775c6855709837297d5dfa8e87fad5
kalin-marinov <kalimar94@gmail.com>
2017-06-11 23:07:56 +0300

    full vs version files

---
+++ Source/frontend/.vs/start.js
@@ -0,0 +1,13 @@
+﻿const exec = require('child_process').exec;
+
+const child = exec('npm start',
+    (error, stdout, stderr) => {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+    });
+
+console.log(`WebPack running on port: ${process.env.port}`);
+`
